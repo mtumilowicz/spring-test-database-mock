48570fe3ec6332efefba6f125eaa8be51ecd4c46
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-01 20:11:11 +0100

    Initial commit

---
+++ src/main/java/com/example/spring/databasemock/DatabasemockApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.databasemock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasemockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasemockApplication.class, args);
+    }
+}
